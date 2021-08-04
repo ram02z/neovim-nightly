60f5b25a597a94939ea4f244cd8a10dad67421b7
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-08-04 22:48:03 +0100

    Attempted fix v4

--- scripts/fix_release.sh
+++ scripts/fix_release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [ ! -f /etc/os-release ]; then
-    echo -ne 'NAME="void"\nID="void"\nDISTRIB_ID="void"\nPRETTY_NAME="void"\n' > /etc/os-release
-fi
+echo -ne 'NAME="void"\nID="void"\nDISTRIB_ID="void"\nPRETTY_NAME="void"\n' > /etc/os-release
+
+cat /etc/os-release
