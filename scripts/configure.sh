b73ead33057c4cecb33a42a76830ac985efecc45
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-08-04 22:40:28 +0100

    Attempted fix v3

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/bin/eho -e '\x1b[32mEnabling ethereal chroot-style...\x1b[0m'
+/bin/echo -e '\x1b[32mEnabling ethereal chroot-style...\x1b[0m'
 echo XBPS_CHROOT_CMD=ethereal >> ./void-packages/etc/conf
 echo XBPS_ALLOW_CHROOT_BREAKOUT=yes >> ./void-packages/etc/conf
 /bin/echo -e '\x1b[32mLinking / to /masterdir...\x1b[0m'
