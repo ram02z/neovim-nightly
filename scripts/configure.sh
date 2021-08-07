e1c8c190cd6cf62e35e5160f223b49e57df7dac0
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-08-07 18:37:58 +0100

    idkkkk

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 /bin/echo -e '\x1b[32mEnabling ethereal chroot-style...\x1b[0m'
-echo XBPS_CHROOT_CMD=ethereal >> ./void-packages/etc/conf
-# echo XBPS_ALLOW_CHROOT_BREAKOUT=yes >> ./void-packages/etc/conf
+# echo XBPS_CHROOT_CMD=ethereal >> ./void-packages/etc/conf
+echo XBPS_ALLOW_CHROOT_BREAKOUT=yes >> ./void-packages/etc/conf
 /bin/echo -e '\x1b[32mLinking / to /masterdir-musl...\x1b[0m'
 ./void-packages/xbps-src -m ./void-packages/masterdir-musl binary-bootstrap x86_64-musl
 # ln -s / ./void-packages/masterdir-musl
