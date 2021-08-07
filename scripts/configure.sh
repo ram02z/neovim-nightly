8537c136573329857336a8e9af936d1d54425581
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-08-07 18:21:27 +0100

    Check output again...

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -3,5 +3,7 @@
 /bin/echo -e '\x1b[32mEnabling ethereal chroot-style...\x1b[0m'
 echo XBPS_CHROOT_CMD=ethereal >> ./void-packages/etc/conf
 echo XBPS_ALLOW_CHROOT_BREAKOUT=yes >> ./void-packages/etc/conf
-/bin/echo -e '\x1b[32mLinking / to /masterdir...\x1b[0m'
-ln -s / ./void-packages/masterdir
+/bin/echo -e '\x1b[32mLinking / to /masterdir-musl...\x1b[0m'
+rm -r ./void-packages/masterdir
+./void-packages/xbps-src -m ./voidpackages/masterdir-musl binary-bootstrap x86_64-musl
+ln -s / ./void-packages/masterdir-musl
