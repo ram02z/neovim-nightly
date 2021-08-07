734ad55424752796875378f3e3e0b641d389a238
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-08-07 19:16:50 +0100

    testing

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -5,6 +5,7 @@ echo XBPS_CHROOT_CMD=ethereal >> ./void-packages/etc/conf
 echo XBPS_ALLOW_CHROOT_BREAKOUT=yes >> ./void-packages/etc/conf
 /bin/echo -e '\x1b[32mLinking / to /masterdir...\x1b[0m'
 ./void-packages/xbps-src zap
+mkdir masterdir/tmp
 ./void-packages/xbps-src binary-bootstrap x86_64-musl
 ln -s / ./void-packages/masterdir
 
