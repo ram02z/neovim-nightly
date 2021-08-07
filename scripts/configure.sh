d7846ad2c2af93d6080a65a1ce80249ef4fbbd63
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-08-07 19:30:18 +0100

    testing4

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -4,6 +4,6 @@
 echo XBPS_CHROOT_CMD=ethereal >> ./void-packages/etc/conf
 echo XBPS_ALLOW_CHROOT_BREAKOUT=yes >> ./void-packages/etc/conf
 /bin/echo -e '\x1b[32mLinking / to /masterdir-musl...\x1b[0m'
-./void-packages/xbps-src -m ./void-packages/masterdir-musl -H ./void-packages/hostdir-musl binary-boostrap x86_64-musl
+./void-packages/xbps-src -m ./void-packages/masterdir-musl -H ./void-packages/hostdir-musl binary-bootstrap x86_64-musl
 ln -s / ./void-packages/masterdir-musl
 
