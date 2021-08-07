be2550e73b073f1dcb02ef43c983c1089ea9a3b5
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-08-07 18:16:00 +0100

    Check output again..

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls -la
-./void-packages/xbps-src -m $dir/masterdir-musl binary-bootstrap x86_64-musl
+sudo cd packages
+./xbps-src -m masterdir-musl binary-bootstrap x86_64-musl
 
 # $dir/xbps-src -m $dir/masterdir-musl pkg neovim-nightly
