e38abb98c962a1f239a494059aa079932e7e02c6
Omar Zeghouani <omarzeghouanii@gmail.com>
2021-08-09 18:43:39 +0100

    back to basics p2

--- Makefile
+++ Makefile
@@ -3,11 +3,14 @@ all: clone bootstrap template build sign
 clone:
 	bash scripts/clone.sh
 
+bootstrap:
+	./void-packages/xbps-src binary-bootstrap
+
 template:
 	bash scripts/template.sh
 
 build:
-	./void-packages/xbps-src -a x86_64-musl pkg neovim-nightly
+	./void-packages/xbps-src pkg neovim-nightly
 
 sign:
 	xbps-rindex --privkey private.pem --sign --signedby "Omar Zeghouani" ./void-packages/hostdir/binpkgs
--- scripts/template.sh
+++ scripts/template.sh
@@ -3,53 +3,53 @@
 dir=$(pwd)/void-packages
 mkdir -p $dir/srcpkgs/neovim-nightly
 ndir=$dir/srcpkgs/neovim-nightly
-# mkdir -p $dir/tmp
-# tmp=$dir/tmp
+mkdir -p $dir/tmp
+tmp=$dir/tmp
 
-# curl \
-#   -H "Accept: application/vnd.github.v3+json" \
-#   -s -o $tmp/release.json \
-#   https://api.github.com/repos/neovim/neovim/releases/tags/nightly
-# version=$(jq -r '.name' $tmp/release.json | sed 's/[^ ]* //')
-# version=${version##*-}
-# url=$(jq -r '.tarball_url' $tmp/release.json)
-# long_commit=$(jq -r '.target_commitish' $tmp/release.json)
+curl \
+  -H "Accept: application/vnd.github.v3+json" \
+  -s -o $tmp/release.json \
+  https://api.github.com/repos/neovim/neovim/releases/tags/nightly
+version=$(jq -r '.name' $tmp/release.json | sed 's/[^ ]* //')
+version=${version##*-}
+url=$(jq -r '.tarball_url' $tmp/release.json)
+long_commit=$(jq -r '.target_commitish' $tmp/release.json)
 
-# echo "version: $version"
-# echo "url: $url" echo "long_commit: $url"
+echo "version: $version"
+echo "url: $url" echo "long_commit: $url"
 
-# builddir=$tmp/build
-# tarfile=$builddir/nvim-$version.tar.gz
-# mkdir -p $builddir
-# echo "tarfile: $tarfile"
+builddir=$tmp/build
+tarfile=$builddir/neovim-nightly-$version.tar.gz
+mkdir -p $builddir
+echo "tarfile: $tarfile"
 
-# wget $url -O $tarfile
-# sha=$(sha256sum $tarfile | awk '{print $1;}')
-# tar xfz $tarfile -C $builddir
-# wrksrc=$(ls $builddir | grep -v '\.')
+wget $url -O $tarfile
+sha=$(sha256sum $tarfile | awk '{print $1;}')
+tar xfz $tarfile -C $builddir
+wrksrc=$(ls $builddir | grep -v '\.')
 
-# echo "sha: $sha"
-# echo "wrksrc: $wrksrc"
+echo "sha: $sha"
+echo "wrksrc: $wrksrc"
 
 cat << EOF > $ndir/template
-# Template file for 'neovim-nightly'
+# Template file for 'neovim-nightly', the nightly build of 'neovim'
 pkgname=neovim-nightly
-version=0.6.0
-revision=1
-wrksrc="neovim-nightly"
+version="$version"
+revision=0
 build_style=cmake
 build_helper="qemu"
 configure_args="-DCMAKE_BUILD_TYPE=RelWithDebInfo"
 hostmakedepends="pkg-config gettext gperf LuaJIT lua51-lpeg lua51-mpack"
-makedepends="libtermkey-devel libuv-devel libvterm-devel msgpack-devel
- LuaJIT-devel libluv-devel tree-sitter-devel"
+makedepends="libtermkey-devel libuv-devel libvterm-devel msgpack-devel LuaJIT-devel
+ libluv-devel tree-sitter-devel"
 depends="libvterm>=0.1.0"
-short_desc="Fork of Vim aiming to improve user experience, plugins and GUIs"
-maintainer="Omar Zeghouani <omarzeghouani@gmail.com>"
+short_desc="Fork of Vim aiming to improve user experience, plugins and GUIs. Nightly version"
+maintainer="Omar Zeghouani <omarzeghouanii@gmail.com>"
 license="Apache-2.0, custom:Vim"
 homepage="https://neovim.io"
-distfiles="https://github.com/neovim/neovim/archive/nightly.tar.gz"
-checksum=e64c87dd660d6a0ad5c0798e4c0e5ca2433f4b99450458e0a4c3cd3c283a913f
+distfiles="$url>neovim-nightly-$version.tar.gz"
+checksum=$sha
+wrksrc=$wrksrc
 alternatives="
  vi:vi:/usr/bin/nvim
  vi:vi.1:/usr/share/man/man1/nvim.1
